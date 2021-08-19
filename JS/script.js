d4721604e4dc9909bd0cf78f942a1c3e5e250104
iscar desktop <scaristhelord@gmail.com>
2021-08-19 20:55:20 +0530

    Finally Completed the best and the hardest part of the project till now

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+let lastKnownScrollPosition = 0;
+let ticking = false;
+
+function doSomething(scrollPos) {
+    let unfor = document.getElementById("unforgettable");
+    let rightSec = unfor.querySelector("#right-sec");
+    let topImg = rightSec.querySelector(".top");
+    let bottomImg = rightSec.querySelector(".bottom");
+    let div = topImg.querySelector("img");
+    let div2 = bottomImg.querySelector("img");
+    div.setAttribute("style","top: 1%");
+    div2.setAttribute("style","top: 26.8%");
+    if(scrollPos!=0){
+        div.setAttribute("style","top: -3.320000001%");
+        div2.setAttribute("style","top: 31%");
+    }
+    // console.log(scrollPos);
+}
+
+document.addEventListener('scroll', function (e) {
+    lastKnownScrollPosition = window.scrollY;
+
+    if (!ticking) {
+        window.requestAnimationFrame(function () {
+            doSomething(lastKnownScrollPosition);
+            ticking = false;
+        });
+
+        ticking = true;
+    }
+});
